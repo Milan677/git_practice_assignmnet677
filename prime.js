38d1435110766bc81ccfa96c1784829a1e1fad77
kamlesh das <daskamlesh677@gmail.com>
2022-11-02 22:42:25 +0530

    check my code

---
+++ prime.js
@@ -0,0 +1,13 @@
+function prime(num){
+ let count=0;
+  for(let i=1;i<=num;i++){
+  if(num%i==0){
+  count++;
+  }
+}
+if(count==0){
+  console.log("prime")
+}else{
+   console.log("not prime")
+}
+}
